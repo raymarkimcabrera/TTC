11d54c73199199d14e027de4088276a081231ae0
Raymarkim Cabrera <raymarkimcabrera@gmail.com>
2020-09-19 17:00:56 +0800

    Remove generated files

--- app/src/main/java/com/ttc/finch_station_app/presentation/splash_screen/SplashScreenActivity.kt
+++ app/src/main/java/com/ttc/finch_station_app/presentation/splash_screen/SplashScreenActivity.kt
@@ -4,11 +4,11 @@ import android.content.Intent
 import com.ttc.finch_station_app.R
 import com.ttc.finch_station_app.base.BaseActivity
 import com.ttc.finch_station_app.presentation.dashboard.DashboardActivity
-import java.util.*
-import kotlin.concurrent.schedule
+import kotlinx.coroutines.*
 
 class SplashScreenActivity : BaseActivity() {
-    var timer = Timer()
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
 
     override fun getLayoutId(): Int = R.layout.activity_splash_screen
 
@@ -18,7 +18,9 @@ class SplashScreenActivity : BaseActivity() {
     }
 
     private fun startTimer() {
-        timer.schedule(3000) {
+        activityScope.launch {
+            delay(3000)
+
             val intent = Intent(this@SplashScreenActivity, DashboardActivity::class.java)
             startActivity(intent)
             finish()
@@ -26,7 +28,7 @@ class SplashScreenActivity : BaseActivity() {
     }
 
     override fun onPause() {
-        timer.cancel()
+        activityScope.cancel()
         super.onPause()
     }
 }
