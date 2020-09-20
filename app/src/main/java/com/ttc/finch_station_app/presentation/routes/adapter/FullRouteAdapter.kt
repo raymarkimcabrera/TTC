aa7efc7b64fafa941b1bf19d0b11cc77ecd1e5e4
Raymarkim Cabrera <raymarkimcabrera@gmail.com>
2020-09-20 21:32:57 +0800

    Show stops and departure time

--- app/src/main/java/com/ttc/finch_station_app/presentation/routes/adapter/FullRouteAdapter.kt
+++ app/src/main/java/com/ttc/finch_station_app/presentation/routes/adapter/FullRouteAdapter.kt
@@ -2,10 +2,15 @@ package com.ttc.finch_station_app.presentation.routes.adapter
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
 import com.jakewharton.rxbinding3.view.clicks
 import com.ttc.finch_station_app.R
+import com.ttc.finch_station_app.extensions.hide
 import com.ttc.finch_station_app.extensions.inflate
+import com.ttc.finch_station_app.extensions.show
 import com.ttc.finch_station_app.model.local.Route
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.row_full_routes.view.*
@@ -45,26 +50,32 @@ class FullRouteAdapter(
             with(itemView) {
 
                 compositeDisposable.add(iv_route_expand.clicks().subscribe {
-//                    if (rl_content.isVisible) {
-//
-//                        TransitionManager.beginDelayedTransition(
-//                            rl_content, AutoTransition()
-//                        )
-//                        rl_content.hide()
-//                        iv_stop_expand.setImageResource(R.drawable.ic_expand_more_black_18dp)
-//                    } else {
-//                        TransitionManager.beginDelayedTransition(
-//                            rl_content, AutoTransition()
-//                        )
-//                        rl_content.show()
-//                        iv_stop_expand.setImageResource(R.drawable.ic_expand_less_black_18dp)
-//                    }
+                    if (ll_stop_route_layout.isVisible) {
+
+                        TransitionManager.beginDelayedTransition(
+                            ll_stop_route_layout, AutoTransition()
+                        )
+                        ll_stop_route_layout.hide()
+                        iv_route_expand.setImageResource(R.drawable.ic_expand_more_black_18dp)
+                    } else {
+                        TransitionManager.beginDelayedTransition(
+                            ll_stop_route_layout, AutoTransition()
+                        )
+                        ll_stop_route_layout.show()
+                        iv_route_expand.setImageResource(R.drawable.ic_expand_less_black_18dp)
+                    }
                 })
             }
         }
 
         fun bindData(item: Route) = with(itemView) {
             tv_route_name.text = item.name
+
+            item.stopTimes?.let {
+                val adapter = StopTimeAdapter(it)
+                rv_stop_times_list.adapter = adapter
+            }
+
         }
     }
 }
---
+++ app/src/main/java/com/ttc/finch_station_app/presentation/routes/adapter/StopTimeAdapter.kt
@@ -0,0 +1,38 @@
+package com.ttc.finch_station_app.presentation.routes.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ttc.finch_station_app.R
+import com.ttc.finch_station_app.extensions.inflate
+import com.ttc.finch_station_app.model.local.StopTime
+import com.ttc.finch_station_app.presentation.dashboard.adapter.StopsViewHolder
+import kotlinx.android.synthetic.main.row_stop_times.view.*
+
+class StopTimeAdapter(private val items: List<StopTime>) :
+    RecyclerView.Adapter<StopTimeAdapter.StopTimeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StopTimeViewHolder =
+        StopTimeViewHolder.create(parent)
+
+    override fun onBindViewHolder(holder: StopTimeViewHolder, position: Int) {
+        holder.bindData(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class StopTimeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        companion object {
+            fun create(parent: ViewGroup): StopTimeViewHolder {
+                return StopTimeViewHolder(parent.inflate(R.layout.row_stop_times))
+            }
+        }
+
+        fun bindData(item: StopTime) = with(itemView) {
+            mtv_stop_route_shape.text = item.shape
+            mtv_departure_time.text = item.departureTime
+        }
+    }
+
+}
